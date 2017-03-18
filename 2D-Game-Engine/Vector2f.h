45a5bc5cfb5db4cebce1d6c3f4b22e794f4e2fd0
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2017-03-18 17:26:36 -0400

    Add vector class

---
+++ 2D-Game-Engine/Vector2f.h
@@ -0,0 +1,31 @@
+#ifndef VECTOR2F_H
+#define VECTOR2F_H
+
+#include "Core.h"
+
+struct Vector2f
+{
+	float32 x = 0;
+	float32 y = 0;
+
+	Vector2f();
+	Vector2f(float32 x, float32 y);
+
+	float32 dot(const Vector2f& v) const;
+	
+	Vector2f normalize() const;
+	Vector2f reverse() const;
+	
+	float32 length() const;
+	float32 lengthSquared() const;
+
+	Vector2f operator+(const Vector2f& v) const;
+	Vector2f operator+(float32 s) const;
+	Vector2f operator-(const Vector2f& v) const;
+	Vector2f operator-(float32 s) const;
+	Vector2f operator*(float32 s) const;
+	Vector2f operator/(float32 s) const;
+};
+
+#endif
+

40c0547d068a8fb2d8e531a7a5a33313ad6e0b4e
Simon <simon.bourque@live.ca>
2017-04-01 20:42:38 -0400

    Add ability to add methods as callbacks instead of functions for input

--- 2D-Game-Engine/Input.cpp
+++ 2D-Game-Engine/Input.cpp
@@ -5,22 +5,37 @@
 Input::Input(Window* window)
 {
 	glfwSetKeyCallback(window->m_handle, keyCallback);
+	glfwSetCursorPosCallback(window->m_handle, cursorPositionCallback);
 }
 
 
 Input::~Input()
 {
+	for (KeyListenerBase* listener : m_keyListeners) {
+		delete listener;
+	}
+
+	for (CursorPositionListenerBase* listener : m_cursorPositionListeners) {
+		delete listener;
+	}
 }
 
-void Input::addKeyListener(void* object, void(*func)(void* object, int32 key, int32 scancode, int32 action, int32 mods)) {
-	m_keyListeners.push_back({ object, func });
-}
-
-void keyCallback(GLFWwindow* window, int32 key, int32 scancode, int32 action, int32 mods) {
+static void keyCallback(GLFWwindow* window, int32 key, int32 scancode, int32 action, int32 mods) {
 	Window* windowPtr = (Window*)glfwGetWindowUserPointer(window);
-	Input* input = (Input*)windowPtr->getInput();
+	Input* input = windowPtr->getInput();
 
-	for (Input::KeyListener listener : input->m_keyListeners) {
-		listener.func(listener.object, key, scancode, action, mods);
+
+	for (Input::KeyListenerBase* listener: input->m_keyListeners) {
+		(*listener)(key, scancode, action, mods);
+	}
+}
+
+static void cursorPositionCallback(GLFWwindow* window, float64 xPos, float64 yPos) {
+	Window* windowPtr = (Window*)glfwGetWindowUserPointer(window);
+	Input* input = windowPtr->getInput();
+
+
+	for (Input::CursorPositionListenerBase* listener : input->m_cursorPositionListeners) {
+		(*listener)(xPos, yPos);
 	}
 }

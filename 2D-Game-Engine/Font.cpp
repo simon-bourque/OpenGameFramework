cb89b6ff632bb870261a549da8fb36f3f2dc6ddd
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2017-05-05 16:31:43 -0400

    Move allocation of Font to FontManager instead of loadFont function

--- 2D-Game-Engine/Font.cpp
+++ 2D-Game-Engine/Font.cpp
@@ -4,7 +4,11 @@
 
 Font::Font(Texture* bitmap, const Glyph& invalidCharacter) : m_bitmap(bitmap), m_invalidCharacter(invalidCharacter) {}
 
-Font::Font(Texture* bitmap, const Glyph& invalidCharacter, const std::map<char, Glyph>& glyphMap) : m_bitmap(bitmap), m_invalidCharacter(invalidCharacter), m_glyphMap(glyphMap) {}
+Font::Font(Texture* bitmap, const Glyph& invalidCharacter, std::pair<char, Glyph>* characterMap, uint32 characterMapSize) : m_bitmap(bitmap), m_invalidCharacter(invalidCharacter) {
+	for (uint32 i = 0; i < characterMapSize; i++) {
+		m_glyphMap[characterMap[i].first] = characterMap[i].second;
+	}
+}
 
 void Font::addCharacterMapping(char c, const Glyph& glyph) {
 	m_glyphMap[c] = glyph;
--- 2D-Game-Engine/Resources.h
+++ 2D-Game-Engine/Resources.h
@@ -2,16 +2,18 @@
 #define RESOURCES_H
 
 #include "Core.h"
+#include <utility>
 
 class RawImage;
 class TileScene;
 class Game;
+class Glyph;
 class Font;
 
 string loadSrc(string file);
 RawImage* loadImage(string file);
 RawImage* loadImages(string file, int32 margin, int32 spacing, int32 tileWidth, int32 tileHeight, int32& imgCount);
-Font* loadFont(string fontAtlas, string fontDesc, Game* game);
+std::pair<char, Glyph>* loadFont(const string& file, uint32& charMapSize, Glyph& invalidCharacter);
 TileScene* loadTileLevel(string file, Game* game);
 
 #endif

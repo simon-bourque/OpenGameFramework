c0c84cb2bfe440c877a6506135922fd548aeb17d
Simon <simon.bourque@live.ca>
2017-05-10 15:27:50 -0400

    Implement creating lvl files

--- Tools/Main.cpp
+++ Tools/Main.cpp
@@ -2,17 +2,32 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
+#include <pugixml.hpp>
+
 #include <Resource/RawImage.h>
 #include <Core/Core.h>
+#include <Scene/Tile.h>
+#include <Math/Geometry/Rectangle.h>
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <ctype.h>
+
+union Integer {
+	int value;
+	char bytes[sizeof(int)];
+};
 
 union UnsignedInteger {
 	unsigned int value;
 	char bytes[sizeof(unsigned int)];
+};
+
+union Float {
+	float value;
+	char bytes[sizeof(float)];
 };
 
 enum Headers : char {
@@ -24,20 +39,23 @@ using namespace std;
 
 int getResponse();
 void choiceTexture();
+void choiceLevel();
 void convertImageToTexture2D(const string& path);
 void choiceSplitInfoFile(const string& path);
 void convertImageToTexture2DArray(const string& path, const string& infoPath, const string& name, const vector<uint32>& indices);
+void convertLevel(const string& path);
 RawImage* loadImage(const string& path);
 
 int main() {
 	cout << "What type of resource would you like to create?" << endl;
 	cout << "1) Texture" << endl;
-	cout << "2) Exit" << endl;
+	cout << "2) Level" << endl;
+	cout << "3) Exit" << endl;
 
 	int response;
 	while (true) {
 		response = getResponse();
-		if (response < 1 || response > 2) {
+		if (response < 1 || response > 3) {
 			cout << "Please enter a valid number." << endl;
 		}
 		else {
@@ -51,6 +69,9 @@ int main() {
 			choiceTexture();
 			break;
 		case 2:
+			choiceLevel();
+			break;
+		case 3:
 			break;
 		default:
 			break;
@@ -99,6 +120,14 @@ void choiceTexture() {
 	default:
 		break;
 	}
+}
+
+void choiceLevel() {
+	cout << "Please enter the path to the .tmx file you wish to convert." << endl;
+	cin >> std::ws;
+	string path;
+	getline(cin, path);
+	convertLevel(path);
 }
 
 void choiceSplitInfoFile(const string& path) {
@@ -289,4 +318,155 @@ void convertImageToTexture2DArray(const string& path, const string& infoPath, co
 	output.close();
 	delete[] imgs;
 	delete img;
+}
+
+void convertLevel(const string& path) {
+	pugi::xml_document doc;
+
+	pugi::xml_parse_result result = doc.load_file(path.c_str());
+
+	if (!result) {
+		cout << "Failed to load tmx file \'" << path << "\'" << endl;
+		return;
+	}
+
+	string originalFileName = path.substr(path.find_last_of("/\\") + 1);
+	string newFileName = originalFileName.substr(0, originalFileName.find_last_of(".")).append(".lvl");
+	string newPath = path.substr(0, path.find_last_of("/\\") + 1).append(newFileName);
+
+	ofstream output(newPath);
+
+	if (!output) {
+		cout << "Failed to write new lvl file." << endl;
+		output.close();
+		return;
+	}
+
+	pugi::xml_node mapNode = doc.child("map");
+	float32 width = mapNode.attribute("width").as_float();
+	float32 height = mapNode.attribute("height").as_float();
+	float32 rectX = (width / 2.0f) - 0.5f;
+	float32 rectY = (height / -2.0f) + 0.5f;
+
+	Float floatUnion;
+
+	floatUnion.value = rectX;
+	output.write(floatUnion.bytes, sizeof(float));
+	floatUnion.value = rectY;
+	output.write(floatUnion.bytes, sizeof(float));
+	floatUnion.value = width;
+	output.write(floatUnion.bytes, sizeof(float));
+	floatUnion.value = height;
+	output.write(floatUnion.bytes, sizeof(float));
+
+	pugi::xml_node dataNode = mapNode.child("layer").child("data");
+	if (string(dataNode.attribute("encoding").value()) != "csv") {
+		cout << "Failed to write new lvl file.  Encoding not supported." << endl;
+		output.close();
+		return;
+	}
+	vector<Tile> tiles;
+	string data = dataNode.child_value();
+	stringstream ss;
+	float32 x = 0;
+	float32 y = 0;
+	for (uint32 i = 0; i < data.size(); i++) {
+		if (isdigit(data[i])) {
+			ss << data[i];
+		}
+		else if (data[i] == ',') {
+			int32 index = stoi(ss.str());
+			index--;
+
+			if (index > -1) {
+				Tile tile(x, -y, index);
+				tiles.push_back(tile);
+			}
+			x++;
+			if (x >= width) {
+				x = 0;
+				y++;
+			}
+			ss.str("");
+		}
+	}
+	int32 index = stoi(ss.str());
+	index--;
+
+	if (index > -1) {
+		Tile tile(x, y, index);
+		tiles.push_back(tile);
+	}
+
+	UnsignedInteger uinteger;
+	Integer integer;
+
+	uinteger.value = tiles.size();
+	output.write(uinteger.bytes, sizeof(unsigned int));
+
+	for (const Tile& tile : tiles) {
+		floatUnion.value = tile.x;
+		output.write(floatUnion.bytes, sizeof(float));
+		floatUnion.value = tile.y;
+		output.write(floatUnion.bytes, sizeof(float));
+		integer.value = tile.index;
+		output.write(integer.bytes, sizeof(int));
+	}
+
+	vector<Rectangle> colliders;
+	pugi::xml_node groupNode = mapNode.child("objectgroup");
+
+	if (string(groupNode.attribute("name").value()) == "collision") {
+		for (auto it = groupNode.begin(); it != groupNode.end(); it++) {
+			float32 collX = it->attribute("x").as_int() / ((float32)mapNode.attribute("tilewidth").as_int());
+			float32 collY = it->attribute("y").as_int() / ((float32)mapNode.attribute("tileheight").as_int());
+			float32 collWidth = it->attribute("width").as_int() / ((float32)mapNode.attribute("tilewidth").as_int());
+			float32 collHeight = it->attribute("height").as_int() / ((float32)mapNode.attribute("tileheight").as_int());
+
+			// Shift origin to center
+			x += (collWidth / 2.0f) - 0.5f;
+			y += (collHeight / 2.0f) - 0.5f;
+
+			Rectangle collider(collX, collY, collWidth, collHeight);
+			colliders.push_back(collider);
+		}
+	}
+
+	uinteger.value = colliders.size();
+	output.write(uinteger.bytes, sizeof(unsigned int));
+
+	for (const Rectangle& collider : colliders) {
+		floatUnion.value = collider.getX();
+		output.write(floatUnion.bytes, sizeof(float));
+		floatUnion.value = collider.getY();
+		output.write(floatUnion.bytes, sizeof(float));
+		floatUnion.value = collider.getWidth();
+		output.write(floatUnion.bytes, sizeof(float));
+		floatUnion.value = collider.getHeight();
+		output.write(floatUnion.bytes, sizeof(float));
+	}
+
+	pugi::xml_node tilesetNode = mapNode.child("tileset");
+	pugi::xml_node imgNode = tilesetNode.child("image");
+	string texturePath = imgNode.attribute("source").value();
+	string textureName = texturePath.substr(texturePath.find_last_of("/\\") + 1);
+	
+	uinteger.value = textureName.size();
+	output.write(uinteger.bytes, sizeof(unsigned int));
+	for (uint32 i = 0; i < textureName.size(); i++) {
+		output.put(textureName[i]);
+	}
+
+	uinteger.value = tilesetNode.attribute("margin").as_uint();
+	output.write(uinteger.bytes, sizeof(unsigned int));
+	uinteger.value = tilesetNode.attribute("spacing").as_uint();
+	output.write(uinteger.bytes, sizeof(unsigned int));
+	uinteger.value = tilesetNode.attribute("tilewidth").as_uint();
+	output.write(uinteger.bytes, sizeof(unsigned int));
+	uinteger.value = tilesetNode.attribute("tileheight").as_uint();
+	output.write(uinteger.bytes, sizeof(unsigned int));
+
+	cout << "Done." << endl;
+
+	output.close();
 }

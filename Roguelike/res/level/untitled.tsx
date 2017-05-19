b96c3533ea2d60a1651d54c7bca752e5b5b21d72
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2017-05-19 17:04:48 -0400

    Add new test level and graphics

--- Roguelike/res/level/untitled.tsx
+++ Roguelike/res/level/untitled.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="dungeontiles-blue" tilewidth="24" tileheight="24" tilecount="60" columns="10">
- <image source="../texture/dungeontiles-blue.png" width="240" height="144"/>
+<tileset name="Floor" tilewidth="16" tileheight="16" tilecount="819" columns="21">
+ <image source="../texture/DawnLike/Objects/Floor.png" width="336" height="624"/>
 </tileset>

a40a5de3f3d8652ad310b326868e18fba370862e
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2017-05-19 19:24:23 -0400

    Add test text

--- Roguelike/RoguelikeGame.cpp
+++ Roguelike/RoguelikeGame.cpp
@@ -6,6 +6,13 @@
 #include "Graphics/Camera.h"
 #include "Object/Transform.h"
 
+#include "Graphics/Text/Font.h"
+#include "Graphics/Text/FontManager.h"
+#include "Graphics/Text/TextManager.h"
+#include "Graphics/Text/Text.h"
+#include "Graphics/Renderer/TextRenderer.h"
+#include "Graphics/Color.h"
+
 #include "RoguePlayer.h"
 
 RoguelikeGame::RoguelikeGame() : Game("Roguelike", 720, 576, Rectangle(20,20)) {}
@@ -20,10 +27,14 @@ void RoguelikeGame::init() {
 	RoguePlayer* player = new RoguePlayer();
 	player->getTransform().translate(54, -53);
 	SceneManager::get()->getCurrentScene().addGameObject(player);
+
+	m_mainFont = RenderSystem::get()->getFontManager()->createFont("const");
+	m_testText = RenderSystem::get()->getTextManager()->createText("test_text_69", "abcdefghijklmnopqrstuvwxyz", m_mainFont, Text::Usage::STATIC);
 }
 
 void RoguelikeGame::render() {
 	Game::render();
+	RenderSystem::get()->getTextRenderer()->renderText(m_testText, -0.9,0.9, Color::WHITE);
 }
 
 void RoguelikeGame::tick(float32 delta) {

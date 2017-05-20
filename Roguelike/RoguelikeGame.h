c080e8405c169a873f21ccad5f17aa953bf9e96c
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2017-05-20 16:50:31 -0400

    More work on ui

--- Roguelike/RoguelikeGame.h
+++ Roguelike/RoguelikeGame.h
@@ -7,13 +7,16 @@
 class Font;
 class Text;
 class UserInterface;
+class InventoryUI;
 class RoguePlayer;
 
 class RoguelikeGame : public Game {
 private:
 	Font* m_mainFont;
 	Text* m_testText;
+
 	UserInterface* m_ui;
+	InventoryUI* m_invUI;
 
 	RoguePlayer* m_player;
 public:

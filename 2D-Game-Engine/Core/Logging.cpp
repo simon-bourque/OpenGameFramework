ffac8f9089534360d793b6b361c178033005e512
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2017-08-18 17:42:57 -0400

    Add function to get current time as a string

--- 2D-Game-Engine/Core/Logging.cpp
+++ 2D-Game-Engine/Core/Logging.cpp
@@ -2,20 +2,9 @@
 #include "Core/Core.h"
 
 #include <iostream>
-#include <ctime>
+
+#include "Core/Time.h"
 
 void printToConsole(const string& message) {
-
-	static time_t currentTimeRaw = 0;
-	static struct tm currentTime;
-	
-	time(&currentTimeRaw);
-	localtime_s(&currentTime, &currentTimeRaw);
-
-	const static int32 dateStrSize = 128;
-	char dateStr[dateStrSize];
-
-	strftime(dateStr, dateStrSize, "[%T] ", &currentTime);
-
-	std::cout << dateStr << message.c_str() << std::endl;
+	std::cout << getSystemTimeString() << ' ' << message.c_str() << std::endl;
 }
--- 2D-Game-Engine/Core/Time.h
+++ 2D-Game-Engine/Core/Time.h
@@ -16,14 +16,40 @@ inline float64 getCurrentTime() {
 	return (time.QuadPart / (float64)frequency.QuadPart);
 }
 
+inline string getSystemTimeString() {
+	WCHAR wtimeStr[32];
+	CHAR timeStr[32];
+	WCHAR* format = L"'['HH':'mm':'ss']'";
+	GetTimeFormatEx(LOCALE_NAME_USER_DEFAULT, TIME_FORCE24HOURFORMAT, NULL, format, wtimeStr, 32);
+	WideCharToMultiByte(CP_ACP, WC_NO_BEST_FIT_CHARS, wtimeStr, -1, timeStr, 32 * sizeof(CHAR), NULL, NULL);
+
+	return string(timeStr);
+}
+
 #endif
 
 #ifdef USING_GLFW_WINDOW
 
 #include <GLFW/glfw3.h>
+#include <ctime>
 
 inline float64 getCurrentTime() {
 	return glfwGetTime();
+}
+
+inline string getSystemTimeString() {
+	time_t currentTimeRaw = 0;
+	struct tm currentTime;
+
+	time(&currentTimeRaw);
+	localtime_s(&currentTime, &currentTimeRaw);
+
+	const static int32 dateStrSize = 128;
+	char dateStr[dateStrSize];
+
+	strftime(dateStr, dateStrSize, "[%T] ", &currentTime);
+
+	return string(dateStr);
 }
 
 #endif

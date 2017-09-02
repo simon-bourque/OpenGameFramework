aedba01f2bff2d9a9e6e88fec4095e6c207edd9c
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2017-09-02 00:51:35 -0400

    Remove functions from Resources.cpp that are no longer needed

--- 2D-Game-Engine/Resource/Resources.h
+++ 2D-Game-Engine/Resource/Resources.h
@@ -13,8 +13,5 @@ class Font;
 string loadSrc(const string& file);
 uint8* loadTexture(const string& file, uint8 type, uint32& width, uint32& height, uint8& channels);
 uint8* loadTexture(const string& file, uint8 type, uint32& width, uint32& height, uint8& channels, uint32& depth);
-RawImage* loadImage(string file);
-RawImage* loadImages(string file, int32 margin, int32 spacing, int32 tileWidth, int32 tileHeight, uint32& imgCount);
-RawImage* loadImages(const string& imgFile, const string& infoFile, uint32& numberOfImages);
 std::pair<char, Glyph>* loadFont(const string& file, uint32& charMapSize, Glyph& invalidCharacter);
 TileScene* loadTileLevel(string file);

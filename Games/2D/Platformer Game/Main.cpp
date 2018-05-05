87f03f6d979c54ad4e7591017c445497c6c46db7
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2018-05-05 17:48:39 -0400

    Remove test assert

--- Games/2D/Platformer Game/Main.cpp
+++ Games/2D/Platformer Game/Main.cpp
@@ -10,7 +10,6 @@
 int main() {
 	Debug::loadSymbols();
 
-	OGF_ASSERT(false, "%s:%f Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",  "THE NUMBER IS", 1.5f);
 	try {
 		PlatformerGame game;
 		game.start();
